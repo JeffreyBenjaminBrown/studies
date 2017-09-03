5f4257f7e8d2b89690f2af3244ce6b35d1a1bce6
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-09-02 23:41:49 -0700

    comment out broken things

--- monad-transformers/src/Two.hs
+++ monad-transformers/src/Two.hs
@@ -11,28 +11,28 @@ import Control.Monad.Trans.Reader (ReaderT(..))
 
 type Env = (Maybe String, Maybe String)
 
-readUserName, readEmailAddress :: MaybeT (ReaderT Env IO) String
-readEmailAddress = readUserName
-readUserName = MaybeT $ ReaderT $ do
-  str <- getLine
-  if length str > 5 then return $ Just str
-                    else return Nothing
-
-ask :: MaybeT (ReaderT Env IO) (String, String)
-ask = do usr <- readUserName -- ** (referred to below)
-         email <- readEmailAddress
-         return (usr, email)
-
-login :: String -> String -> IO ()
-login a b = putStrLn $ "Hello " ++ a ++ ", your email is " ++ b ++ "."
-
-readUserName' :: MaybeT (ReaderT Env IO) String
-readUserName' = MaybeT $ do
-  (maybeOldUser, _) <- ask
-  case maybeOldUser of
-    Just str -> return str
-    Nothing -> do 
-      input <- lift getLine -- lift allows IO from inside ReaderT Env IO
-      if length input > 5
-        then return (Just input)
-        else return Nothing
+--readUserName, readEmailAddress :: MaybeT (ReaderT Env IO) String
+--readEmailAddress = readUserName
+--readUserName = MaybeT $ ReaderT $ do
+--  str <- getLine
+--  if length str > 5 then return $ Just str
+--                    else return Nothing
+--
+--ask :: MaybeT (ReaderT Env IO) (String, String)
+--ask = do usr <- readUserName -- ** (referred to below)
+--         email <- readEmailAddress
+--         return (usr, email)
+--
+--login :: String -> String -> IO ()
+--login a b = putStrLn $ "Hello " ++ a ++ ", your email is " ++ b ++ "."
+--
+--readUserName' :: MaybeT (ReaderT Env IO) String
+--readUserName' = MaybeT $ do
+--  (maybeOldUser, _) <- ask
+--  case maybeOldUser of
+--    Just str -> return str
+--    Nothing -> do 
+--      input <- lift getLine -- lift allows IO from inside ReaderT Env IO
+--      if length input > 5
+--        then return (Just input)
+--        else return Nothing
